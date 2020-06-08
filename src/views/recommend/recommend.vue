a3c98964a1ec36bee3da5c28866057a06e095527
Xinghui_Wang <wang_xh88@163.com>
2020-06-08 15:27:20 +0800

    swiper 组件完成

--- src/views/recommend/recommend.vue
+++ src/views/recommend/recommend.vue
@@ -1,14 +1,70 @@
 <template>
-  <div style="color: #fff">
-    推荐页面
+  <div class="recommend">
+    <div class="recommend-content">
+      <div class="slider-wrapper" v-if="recommends.length">
+        <slider>
+          <div v-for="item in recommends" :key="item.id" class="slider-item">
+            <a :href="item.linkUrl">
+              <img class="needsclick" :src="item.picUrl">
+            </a>
+          </div>
+        </slider>
+      </div>
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌单推荐</h1>
+        <ul></ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import { getRecommend } from 'api/recommend'
+import { ERR_OK } from 'api/config'
+import Slider from 'components/slider/slider'
+
 export default {
-  name: 'Recommend'
+  name: 'Recommend',
+  data () {
+    return {
+      recommends: []
+    }
+  },
+  components: {
+    Slider
+  },
+  created () {
+    this._getRecommend()
+  },
+  methods: {
+    _getRecommend () {
+      getRecommend().then((res) => {
+        if (res.code === ERR_OK) {
+          this.recommends = res.data.slider
+        }
+      })
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped rel="stylus/stylesheet">
+  @import '~common/stylus/variable'
+  .recommend
+    width 100%
+    .recommend-content
+      height 100%
+      overflow hidden
+      .slider-wrapper
+        overflow hidden
+        height 0
+        width 100%
+        padding-bottom 40%
+      .recommend-list
+        .list-title
+          height 65px
+          line-height 65px
+          text-align center
+          font-size $font-size-medium
+          color $color-theme
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
+const ERR_OK = 0
 const path = require('path')
+const axios = require('axios')
 
 function resolve (dir) {
   return path.join(__dirname, dir)
@@ -11,5 +13,47 @@ module.exports = {
       .set('api', resolve('src/api'))
       .set('components', resolve('src/components'))
       .set('views', resolve('src/views'))
+  },
+  devServer: {
+    before (app) {
+      app.get('/api/getTopBanner', function (req, res) {
+        const url = 'https://u.y.qq.com/cgi-bin/musics.fcg'
+        const RecommendlinkUrl = 'https://y.qq.com/'
+
+        axios.get(url, {
+          headers: {
+            referer: 'https://y.qq.com/',
+            host: 'u.y.qq.com'
+          },
+          params: req.query
+        }).then((response) => {
+          response = response.data
+          if (response.code === ERR_OK) {
+            const slider = []
+            const content = response.focus.data.shelf.v_niche[0].v_card
+            if (content) {
+              for (let i = 0; i < content.length; i++) {
+                const item = content[i]
+                const sliderItem = {}
+                sliderItem.id = item.id + i
+                sliderItem.linkUrl = RecommendlinkUrl
+                sliderItem.picUrl = item.cover
+                slider.push(sliderItem)
+              }
+            }
+            res.json({
+              code: 0,
+              data: {
+                slider
+              }
+            })
+          } else {
+            res.json(response)
+          }
+        }).catch((e) => {
+          console.log(e)
+        })
+      })
+    }
   }
 }
